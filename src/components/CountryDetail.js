40cbcd950e7c1feb330f47abad739767afd68d97
André Torres <andretorresjava@gmail.com>
2022-06-14 21:50:24 -0500

    Adding styles to the CountryDetail component

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,49 +1,62 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 
 const CountryDetail = () => {
   const navigate = useNavigate();
   const { countryId } = useParams();
   const { state } = useFetch();
-  let result;
 
-  if(state.length){
-    result = state.filter(country => country.name === countryId)[0];
-  }
+  if(!state.length) return;
+  let result = state.filter(country => country.name === countryId)[0];
+  const { flags:{ png }, name, nativeName, population, region, subregion, capital, topLevelDomain, currencies, languages, borders } = result;
 
   return (
     <main className="main-country-detail">
         <div className="container">
             <button 
               className="return-home"
-              onClick={() => {navigate("/countries")}}
+              onClick={() => navigate("/countries")}
             >
-              <FontAwesomeIcon icon={faArrowLeft} />
+              <FontAwesomeIcon icon={faArrowLeftLong} />
               <span className="return">Back</span>
             </button>
             <div className="country">
               <img 
                 className="flag"    
-                src={result?.flags.png}
-                alt={result?.name}
+                src={png}
+                alt={name}
               />
               <div className="data">
-                <h2 className="name">{result?.name}</h2>
+                <h2 className="name">{name}</h2>
                 <div className="features">
                   <ul className="main">
-                    <li className="feature">native name: <span className="span">{result?.nativeName}</span></li>
-                    <li className="feature">population: <span className="span">{result?.population.toLocaleString('en-US')}</span></li>
-                    <li className="feature">region: <span className="span">{result?.region}</span></li>
-                    <li className="feature">sub region: <span className="span">{result?.subregion}</span></li>
-                    <li className="feature">capital: <span className="span">{result?.subregion}</span></li>
+                    <li className="feature">native name: <span className="span">{nativeName}</span></li>
+                    <li className="feature">population: <span className="span">{population.toLocaleString('en-US')}</span></li>
+                    <li className="feature">region: <span className="span">{region}</span></li>
+                    <li className="feature">sub region: <span className="span">{subregion}</span></li>
+                    <li className="feature">capital: <span className="span">{capital}</span></li>
                   </ul>
                   <ul className="secondary">
-                    <li className="feature">top level domain: <span className="span">{result?.topLevelDomain[0]}</span></li>
-                    <li className="feature">currencies: <span className="span">{result?.currencies[0].name}</span></li>
-                    <li className="feature">languages: <span className="span">{result?.languages.map(lenguaje => (lenguaje.name))}</span></li>
+                    <li className="feature">top level domain: <span className="span">{topLevelDomain[0]}</span></li>
+                    <li className="feature">currencies: <span className="span">{currencies && currencies[0].name}</span></li>
+                    <li className="feature">languages: <span className="span">{languages.map( language => ( language.name )).join(', ')}</span></li>
                   </ul>
+                </div>
+                <div className="border-countries">
+                  <span className="span">border countries</span>
+                  <div className="borders">
+                    {
+                      borders?.map((border, index) => (
+                        <Link 
+                          className="border"
+                          key={index}
+                          to={`/countries/${countryId}`}
+                        >{border}</Link>
+                      ))
+                    }
+                  </div>
                 </div>
               </div>
             </div>
